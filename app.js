d8f541d982d5e8ba5943f37cb5e1e16cfc997566
wami <wamiwami@live.jp>
2016-10-09 00:18:52 +0900

    panel対応

--- app.js
+++ app.js
@@ -27,27 +27,45 @@ $light.addEventListener("click", toggelClass, false);
 let milkcocoa = new MilkCocoa(`dogi9jz8c16.mlkcca.com`);
 const dot_ds = milkcocoa.dataStore('dots');
 //Nefry側のアプリ&データストア
-milkcocoa = new MilkCocoa(`woodilrg1cz3.mlkcca.com`);
-const nefry_ds = milkcocoa.dataStore('nefry');
+let milkcocoa2 = new MilkCocoa(`readidztqm2w.mlkcca.com`);
+const nefry_ds = milkcocoa2.dataStore('nefry');
 
-let flag = 0, red = 0, green = 0, blue = 0;
+let flag = 0,color=0;
+let position=0;
 dot_ds.on('push',(pushed) => {
     //ドット絵から送られてくるカラーコードをRGBに変換
-    let color = pushed.value.color;
+    color = pushed.value.color;
     red = parseInt(color.substr(1,2), 16);
     green = parseInt(color.substr(3,2), 16);
     blue = parseInt(color.substr(5,2), 16);
 
-    addClass(color);
+    position = pushed.value.index;
     flag = 1;
-
-    //ドット絵から受け取ったデータをNefryに流す
-    nefry_ds.send({r:red,g:green,b:blue},(err,pushed) => {
-        console.log(pushed);
-    });
+    for(var i=0;i<16;i++){
+        if(position<56){
+        if(position<16){
+            if(i!=0&&i%2){
+                position=15-position;
+            }
+            position+=16*i;
+            addClass(color);
+            console.log(position);
+                //ドット絵から受け取ったデータをNefryに流す
+            nefry_ds.push({r:red,g:green,b:blue,ps:position},(err,pushed) => {
+                console.log(pushed);
+            });
+            console.log(position);
+            break;
+        }
+        break;
+        }
+        if(position=>56){
+            position-=56;
+        }
+    }
 });
 
 setInterval(()=>{
     removeClass();
     flag = 0;
-},5000);
+},1000);
